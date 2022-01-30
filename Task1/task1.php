3ca35fadb0c1251a55c010a5ec1bbc83d6586e77
sazzadulalambd <sazzad.sua@gmail.com>
2022-01-30 12:44:13 +0600

    first commit

---
+++ Task1/task1.php
@@ -0,0 +1,72 @@
+<!--shawon,sazzadul alam [19-39385-1]-->
+
+<html>
+<head>
+<title>  Task 1  </title> 
+</head>
+
+<body>
+    <h1>Registration From <hr></h1>
+    <form action = "https://github.com/sazzadulalambd/WT_Spring2022/">
+    <table>
+
+<tr>
+    <td>Frist Name:</td>
+    <td><input type ="text" name ="f_name"></td>
+</tr>
+
+<tr>
+    <td>Last Name:</td>
+    <td><input type ="text" name ="l_name"></td>
+</tr>
+
+<tr>
+    <td>Age:</td>
+    <td><input type ="text" name ="a_name"></td>
+</tr>
+
+<tr>
+    <td>Designation:</td>
+    <td><input type ="radio" name="Designation" >Junior Programmer
+        <input type ="radio" name="Designation" >Senior Programmer
+        <input type ="radio" name="Designation" >Project Lead
+    </td>
+</tr>
+
+<tr>
+    <td>Preferred language</td>
+    <td><input type ="checkbox" name="Preferred_ language" > JAVA
+        <input type ="checkbox" name="Preferred_ language" > PHP
+        <input type ="checkbox" name="Preferred_ language" > C++
+    </td>
+</tr>
+
+<tr>
+    <td>E-mail:</td>
+    <td><input type ="text" name ="ename"></td>
+</tr>
+
+<tr>
+    <td>Password:</td>
+    <td><input type="password" value="FakePSW"></td>
+</tr>
+
+<tr>
+    <td>Please choose a file </td>
+    <td><input type="file" name="myfile"></td>
+</tr>
+
+<tr>
+    <td><input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+    </td>
+    
+</tr>
+       
+</table>
+
+</form>
+
+</body>
+
+</html>
